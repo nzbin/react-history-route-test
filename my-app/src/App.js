da4e1f7aa5739f5729e78069964088677320506c
nzbin <nzb329@163.com>
2020-12-02 15:41:56 +0800

    refactor: add my-app

---
+++ my-app/src/App.js
@@ -0,0 +1,28 @@
+import Navbar from './components/Navbar';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div className="container mt-2" style={{ marginTop: 40 }}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/profile/:name">
+            <Profile />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
